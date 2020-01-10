4a055b4176f1621f39b23b47ffe25fc16537dcba
atsushi-h <atsushi.1213eng@gmail.com>
2020-01-11 07:17:55 +0900

    UNIT2 LESSON8

---
+++ unit_2/lesson_8/first_express_project/main.js
@@ -0,0 +1,10 @@
+const port = 3000;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Universe!");
+})
+.listen(port, () => {
+  console.log(`The Express.js server has started and is listening on port number: ${port}`);
+});
